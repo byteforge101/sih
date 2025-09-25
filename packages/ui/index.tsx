efb9983cc0da067bd9986d548f5c643ecfdd614f
ANIKET NAG <150041396+homelesssnake-101@users.noreply.github.com>
2025-09-25 17:22:00 +0530

    Add community project exports to index.tsx
    Added exports for MentorCommunityProjects and StudentCommunityProjects.

--- packages/ui/index.tsx
+++ packages/ui/index.tsx
@@ -5,4 +5,7 @@ export * from "./src/EnrollStudentFace"; // <-- Add this line
 export * from "./src/FaceRecognizer"; // <-- Add this line
 export * from "./src/AttendanceList";
 export * from "./src/CurricularUnitForm";
+
 export * from "./src/Chatbot";
+export { default as MentorCommunityProjects } from './src/community/MentorCommunityProjects';
+export { default as StudentCommunityProjects } from './src/community/StudentCommunityProjects';
